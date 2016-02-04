5821ee4993459b93d34190f284b99e5f6e82a96a
lalwr <fsali@naver.com>
2016-02-04 13:43:46 +0900

    알고리즘 문제 풀기

---
+++ acmicpc_10951/src/Main.java
@@ -0,0 +1,23 @@
+/**
+ * Created by admin on 2016-02-04.
+ * https://www.acmicpc.net/problem/10951 A+B -4
+ */
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        while(sc.hasNextInt()){
+            a = sc.nextInt();
+            b = sc.nextInt();
+            System.out.println(a+b);
+
+        }
+
+
+    }
+
+
+}
