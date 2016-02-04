c781fde11d61bde500a6fa5fc32834402fcc2ad7
lalwr <fsali@naver.com>
2016-02-04 17:22:09 +0900

    알고리즘 문제 풀기

---
+++ acmicpc_10757/src/Main.java
@@ -0,0 +1,22 @@
+/**
+ * Created by admin on 2016-02-04.
+ * https://www.acmicpc.net/problem/10757 큰 수 A+B
+ */
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        BigInteger a = new BigInteger(sc.next());
+        BigInteger b = new BigInteger(sc.next());
+
+        System.out.println(a.add(b));
+
+
+    }
+
+
+}
