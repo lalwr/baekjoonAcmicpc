2545fba58564ab2bb98107dad573ec466ff966d2
lalwr <fsali@naver.com>
2016-02-04 16:59:45 +0900

    알고리즘 문제 풀기

---
+++ acmicpc_2558/src/Main.java
@@ -0,0 +1,21 @@
+/**
+ * Created by admin on 2016-02-04.
+ */
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.println(a+b);
+
+
+    }
+
+
+}
